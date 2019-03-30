11f6871e845f1644b06bfa00e69f2fff44b0b10d
JP Kramer <jpkramer707_github@mm.st>
2019-03-30 12:59:45 -0700

    Linked Tags to TagDetail view

--- src/Tag.js
+++ src/Tag.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Tag = ({tag}) => {
 	return (
-	  <li className={ 'tag_' + tag.id }>
-	  	{ tag.name }
-	  </li>
+		<li className={ 'tag_' + tag.id }>
+			<Link to={ '/tags/' + tag.id }>{ tag.name }</Link>
+		</li>
 	);
 };
 
