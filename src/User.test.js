97e62fe47f03dabc38941b364bd0797b0c99f9e6
JP Kramer <jpkramer707_github@mm.st>
2019-04-04 15:23:06 -0700

    Added Enzyme, Chai, User test, UserList test

--- src/User.test.js
+++ src/User.test.js
@@ -2,33 +2,46 @@ import React from "react";
 import Enzyme, { mount } from "enzyme";
 import User from "./User.js";
 import Adapter from 'enzyme-adapter-react-16';
+import { userData } from './data/TestData.js';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("User", () => {
-  let props,
-    mountedUser;
-  const user = () => {
+  let props, mountedUser;
+
+  const user = (user, me) => {
     if (!mountedUser) {
       mountedUser = mount(
-        <User {...props} />
+        <User {...props} user={user} me={me} />
       );
     }
     return mountedUser;
   }
 
   beforeEach(() => {
-    props = {
-      user: {
-        avatar: 'avatar',
-        tags: []
-      }
-    };
+    props = {};
     mountedUser = undefined;
   });
   
-  it("always renders a div", () => {
-    const divs = user().find("div");
-    expect(divs.length).toBeGreaterThan(0);
+  it("recognizes current user", () => {
+    expect(
+      user(
+        userData[0],
+        userData[0]
+      )
+      .find("li")
+      .hasClass('me')
+    ).toEqual(true);
+  });
+
+  it("does not call other users 'me'", () => {
+    expect(
+      user(
+        userData[0],
+        userData[1]
+      )
+      .find("li")
+      .hasClass('me')
+    ).toEqual(false);
   });
 });
---
+++ src/data/TestData.js
@@ -0,0 +1,8 @@
+export const
+	userData = [
+	  { id: 0, avatar: 'avatar1', tags: [] },
+	  { id: 1, avatar: 'avatar2', tags: [] },
+	  { id: 2, avatar: 'avatar3', tags: [] },
+	  { id: 3, avatar: 'avatar4', tags: [] },
+	  { id: 4, avatar: 'avatar5', tags: [] },
+	];
