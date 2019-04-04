c37387494d2724d63036e2f5aa652f6218ddc667
JP Kramer <jpkramer707_github@mm.st>
2019-04-04 13:19:47 -0700

    Privacy

--- src/data/DefaultState.js
+++ src/data/DefaultState.js
@@ -1,3 +1,17 @@
+import pluck from 'utils-pluck';
+
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i == 0 ) return this;
+  while ( --i ) {
+	 j = Math.floor( Math.random() * ( i + 1 ) );
+	 temp = this[i];
+	 this[i] = this[j];
+	 this[j] = temp;
+  }
+  return this;
+}
+
 const DefaultState = {
   testsLoaded: false,
   testLoadError: false,
@@ -11,12 +25,13 @@ const DefaultState = {
   userTags: [],
   testTags: [],
   tests: [],
+  userRelationships: [],
   privacyLevels: [
-	{ value: 0, name: "Private" },
-	{ value: 1, name: "Close" },
-	{ value: 2, name: "Near" },
-	{ value: 3, name: "Far" },
-	{ value: 4, name: "Public" },
+	{ id: 0, value: 0, name: "Private" },
+	{ id: 1, value: 1, name: "Close" },
+	{ id: 2, value: 2, name: "Near" },
+	{ id: 3, value: 3, name: "Far" },
+	{ id: 4, value: 4, name: "Public" },
   ]
 };
 
@@ -28,8 +43,14 @@ for (var userId=1; userId<22; userId++) {
 		id: userId, name: "Anon #"+userId, avatar: '/userAvatar_'+Math.floor(Math.random()*5)
 	};
 	for (var tagId=1; tagId<Math.floor(Math.random()*10); tagId++) {
+		var privacyLevel = (
+			tagId === 0 ? 4 : pluck(DefaultState.privacyLevels, 'id').shuffle().splice(0,1)[0]
+		);
+		privacyLevel = 4;
 		fauxUserTags.push({
-			userId: userId, tagId: Math.floor(Math.random()*10)
+			userId: userId,
+			tagId: Math.floor(Math.random()*10),
+			privacyLevel: privacyLevel
 		})
 	}
 	DefaultState.users.push(fauxUser);
