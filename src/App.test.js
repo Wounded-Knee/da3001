433874693553d2c4f6c87e6b421e771812bf01ce
JP Kramer <jpkramer707_github@mm.st>
2019-04-04 19:48:00 -0700

    Cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,11 @@ import React from "react";
 import Enzyme, { mount } from "enzyme";
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
-import { userData } from './data/TestData.js';
 import { expect } from 'chai';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("App", () => {
-	let component,
-		instance,
-		appWillLoad;
-
 	const getComponent = (props) => {
 		return new Promise((resolve, reject) => {
 			var component;
@@ -22,8 +17,6 @@ describe("App", () => {
 	}
 
 	beforeEach(() => {
-		appWillLoad = undefined;
-		instance = undefined;
 	});
 	
 	it("getTestById()", () => {
--- src/TagDetail.js
+++ src/TagDetail.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import UserList from './UserList.js';
 import TagList from './TagList.js';
-import Tag from './Tag.js';
 import HDiv from './HDiv.js';
 
 const TagDetail = (props) => {
