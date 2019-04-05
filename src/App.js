e1896e360b04efc67236b4631d7e1954d25da956
JP Kramer <jpkramer707_github@mm.st>
2019-04-05 15:00:34 -0700

    Code cleanup - Removed client-side API/Database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import UserList from './UserList.js';
-import TestPromises from './TestPromises.js';
 import TagDetail from './TagDetail.js';
 import TestList from './TestList.js';
 import PrivacyLayout from './PrivacyLayout.js';
 import DefaultState from './data/DefaultState.js';
-import pluck from 'utils-pluck';
 import './App.css';
 
 class App extends Component {
@@ -16,245 +14,8 @@ class App extends Component {
 		// Setup state
 		this.state = DefaultState;
 
-		// Handle promises for test module loading
-		this.awaitTestPromises();
-
 		// Scope pointer for debuggery
 		setTimeout(() => window.da3001 = this, 500);
-	}
-
-	awaitTestPromises() {
-		const onImportAllTests = testPromises => {
-			this.setState((previousState, currentProps) => {
-				if (this.props.onLoad) this.props.onLoad();
-				var testId = previousState.tests.length;
-				return {
-					tests: testPromises ? testPromises.map((testPromise, index) => ({
-						id: testId + index,
-						TestClass: testPromise.default,
-						testInstance: undefined,
-					})) : [],
-					testsLoaded: !!testPromises,
-					testLoadError: !testPromises
-				};
-			});
-		};
-
-		Promise.all(TestPromises).then(
-			onImportAllTests
-		).catch(
-			onImportAllTests
-		);
-	}
-
-	initializeTest({ TestClass, testInstance }) {
-		pluck(testInstance.getAnswers(), 'tag').map((tagName, index) => {
-			return this.createTag(
-				tagName,
-				globalTagId => {
-					testInstance.assignGlobalIdToTag(index, globalTagId);
-					this.setState((previousState, currentProps) => {
-						return {
-							...previousState,
-							tests: [
-								...(previousState.tests.map(test => ({
-									...test,
-									testInstance: (test.TestClass === TestClass) ? testInstance : test.testInstance
-								})))
-							],
-							testTags: [
-								...previousState.testTags,
-								{
-									testId: this.getIdByTestClass(TestClass),
-									tagId: globalTagId
-								}
-							]
-						}
-					});
-				}
-			)
-		});
-	}
-
-	/* Tests
-	*********/
-	getIdByTestClass(TestClass) {
-		return this.state.tests.filter(test => test.TestClass === TestClass)[0].id;
-	}
-
-	getTestById(testId) {
-		return this.state.tests.filter(test => test.id === testId)[0];
-	}
-
-	getTestByTag(tag) {
-		return this.getTestById(this.state.testTags.filter(testTag => testTag.tagId === tag.id)[0].testId);
-	}
-
-	/* Tags
-	********/
-	createTag(tagName, registerNewTagIdCallback) {
-		this.setState((previousState, currentProps) => {
-			const globalTagId = Math.max(...pluck(previousState.tags, 'id'), -1)+1;
-			registerNewTagIdCallback(globalTagId);
-			return {
-				...previousState,
-				tags: [
-					...previousState.tags,
-					{
-						id: globalTagId,
-						name: tagName
-					}
-				]
-			};
-		});
-	}
-
-	getTagById(globalTagId) {
-		return this.getTagsById([ parseInt(globalTagId) ])[0];
-	}
-
-	getTagsById(globalTagIds) {
-		return this.hydrateTags(this.state.tags.filter(tag => globalTagIds.indexOf(tag.id) !== -1));
-	}
-
-	hydrateTags(tags) {
-		return tags.map(tag => ({
-			...tag,
-			test: this.getTestByTag(tag)
-		}));
-	}
-
-	getTagsByTest(test) {
-		return this.hydrateTags(
-			this.getTagsById(
-				pluck(
-					this.state.testTags.filter(testTag => testTag.testId === test.id),
-					'tagId'
-				)
-			)
-		);
-	}
-
-	getObserverPrivacylevel(observedUser, observingUser=this.state.me) {
-		const relationship = observedUser.relationships.filter(
-			relationship => (
-				relationship.relationship_id === observingUser.id
-			)
-		)[0];
-		return relationship ? (
-			this.state.privacyLevels[relationship.privacyLevelId]
-		) : this.state.privacyLevels[4];
-	}
-
-	observableUserTags(observedUser, observingUser=this.state.me) {
-		return this.state.tags.filter(
-			({ id, name }) => (
-				pluck(
-					this.state.userTags.filter(
-						({ userId, tagId, privacyLevelId }) => (
-							userId === observedUser.id &&
-							(
-								privacyLevelId >= this.getObserverPrivacylevel(observedUser, observingUser).id ||
-								userId === observingUser.id
-							)
-						)
-					),
-					'tagId'
-				).indexOf(id) !== -1
-			)
-		);
-	}
-
-	/* Users
-	********/
-	getUsers() {
-		return this.hydrateUsers(this.state.users);
-	}
-
-	getUserById(id) {
-		return this.getUsersById([id])[0];
-	}
-
-	getUsersById(ids) {
-		return this.hydrateUsers(this.state.users.filter(( user ) => ( ids.indexOf(user.id) !== -1 )));
-	}
-
-	getMe() {
-		return this.getUserById(this.state.me.id);
-	}
-
-	/* User Tags
-	************/
-	hydrateUsers(users) {
-		return users.map(user => {
-			var hydratedUser = { ...user };
-			hydratedUser.relationships = this.state.userRelationships.filter(relationship => (
-				relationship.userId === user.id
-			));
-			hydratedUser.tags = this.observableUserTags(hydratedUser)
-			return hydratedUser;
-		});
-	}
-
-	giveTagToMe(globalTagId) {
-		this.giveTagToUser(this.getMe(), globalTagId);
-	}
-
-	giveTagToUser(user, globalTagId) {
-		this.setState((previousState, currentProps) => {
-			return {
-				...previousState,
-				userTags: [
-					...previousState.userTags,
-					{
-						userId: user.id,
-						tagId: globalTagId
-					}
-				]
-			}
-		})
-	}
-
-	/*
-	 * Receives an array of globalTagIds,
-	 * removes globalTagIds which the targeted user does not have,
-	 * returns the changed array
-	 *
-	 * Except it does not change the array, it creates a new one.
-	 **/
-	tagsUserHas(user, globalTagIds) {
-		// Todo: Limit number of id checks for privacy
-		const userTags = pluck(user.tags, 'id');
-		return globalTagIds.filter(tagId => userTags.indexOf(tagId) !== -1);
-	}
-
-	tagsIHave(globalTagIds) {
-		return this.tagsUserHas(this.getMe(), globalTagIds);
-	}
-
-	usersWhoHaveTag(globalTagId) {
-		const userTags = this.state.userTags.filter(userTag => userTag.tagId === parseInt(globalTagId));
-		const idsOfUsersWhoHaveTheTag = pluck(userTags, 'userId');
-		const usersWhoHaveTag = this.getUsersById(idsOfUsersWhoHaveTheTag);
-		return usersWhoHaveTag;
-	}
-
-	/* Misc
-	********/
-	recordAnswer(answer) {
-		this.giveTagToMe(answer.globalTagId);
-	}
-
-	/* Render Helpers
-	***************/
-	shouldTestRender(test) {
-		const tagsInTest = pluck(test.getAnswers(), 'globalTagId');
-		const dependsOnTags = test.dependsOnTags();
-		return (
-			test.shouldTestRender(this.tagsIHave(dependsOnTags)) &&
-			this.tagsIHave(tagsInTest).length === 0 &&
-			true
-		);
 	}
 
 	getRoutes() {
@@ -273,30 +34,15 @@ class App extends Component {
 					}
 				/>
 
+				{/* --- Privacy View --- */}
 				<Route
 					path="/privacy"
 					render={
 						routeProps => <PrivacyLayout
 							{...routeProps}
 							privacyLevels={ this.state.privacyLevels }
-							users={
-								[
-									this.getUsers().shuffle().splice(0,3),
-									this.getUsers().shuffle().splice(0,6),
-									this.getUsers().shuffle().splice(0,13),
-									this.getUsers().shuffle().splice(0,23),
-									this.getUsers().shuffle().splice(0,30),
-								]
-							}
-							tags={
-								[
-									this.getMe().tags.splice(0,15),
-									this.getMe().tags.splice(0,8),
-									this.getMe().tags.splice(0,7),
-									this.getMe().tags.splice(0,5),
-									this.getMe().tags.splice(0,3),
-								]
-							}
+							users={[]}
+							tags={[]}
 						/>
 					}
 				/>
@@ -357,15 +103,3 @@ class App extends Component {
 }
 
 export default App;
-
-Array.prototype.shuffle = function() {
-  var i = this.length, j, temp;
-  if ( i == 0 ) return this;
-  while ( --i ) {
-	 j = Math.floor( Math.random() * ( i + 1 ) );
-	 temp = this[i];
-	 this[i] = this[j];
-	 this[j] = temp;
-  }
-  return this;
-}
--- src/data/DefaultState.js
+++ src/data/DefaultState.js
@@ -1,25 +1,11 @@
-import pluck from 'utils-pluck';
-
-Array.prototype.shuffle = function() {
-	var i = this.length, j, temp;
-	if ( i == 0 ) return this;
-	while ( --i ) {
-		j = Math.floor( Math.random() * ( i + 1 ) );
-		temp = this[i];
-		this[i] = this[j];
-		this[j] = temp;
-	}
-	return this;
-}
-
 const DefaultState = {
-	testsLoaded: false,
-	testLoadError: false,
+	loadStatus: {
+		app: false,
+		tests: false,
+	},
 	me: {
-	id: 0
 	},
 	users: [
-	{ id: 0, name: 'Anonymous', avatar: '/userAvatar_0' }
 	],
 	tags: [],
 	userTags: [],
@@ -27,34 +13,12 @@ const DefaultState = {
 	tests: [],
 	userRelationships: [],
 	privacyLevels: [
-	{ id: 0, value: 0, name: "Private" },
-	{ id: 1, value: 1, name: "Close" },
-	{ id: 2, value: 2, name: "Near" },
-	{ id: 3, value: 3, name: "Far" },
-	{ id: 4, value: 4, name: "Public" },
+		{ id: 0, value: 0, name: "Private" },
+		{ id: 1, value: 1, name: "Close" },
+		{ id: 2, value: 2, name: "Near" },
+		{ id: 3, value: 3, name: "Far" },
+		{ id: 4, value: 4, name: "Public" },
 	]
-};
-
-// Temporarily generating fake users
-var fauxUser,
-	fauxUserTags = [];
-for (var userId=1; userId<22; userId++) {
-	fauxUser = {
-		id: userId, name: "Anon #"+userId, avatar: '/userAvatar_'+Math.floor(Math.random()*5)
-	};
-	for (var tagId=1; tagId<Math.floor(Math.random()*10); tagId++) {
-		var privacyLevel = (
-			tagId === 0 ? 4 : pluck(DefaultState.privacyLevels, 'id').shuffle().splice(0,1)[0]
-		);
-		privacyLevel = 4;
-		fauxUserTags.push({
-			userId: userId,
-			tagId: Math.floor(Math.random()*10),
-			privacyLevel: privacyLevel
-		})
-	}
-	DefaultState.users.push(fauxUser);
-	DefaultState.userTags = [...DefaultState.userTags, ...fauxUserTags];
 };
 
 export default DefaultState;
